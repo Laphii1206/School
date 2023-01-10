362a3906b876b087ab29836060b47043f7523fcc
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-01-11 02:38:52 +0800

    Add files via upload

---
+++ school monkaS/Project1/Week 1.cpp
@@ -0,0 +1,17 @@
+// header
+#include <iostream>
+using namespace std;
+
+		int main() {
+		int age;
+		cout << "How old are you\n";
+		cin >> age;
+		cout << "you are " << age << " years old";
+		return 0;
+}
+
+
+/*		int firstCode() {
+			cout << "hello!";
+				return 0;
+		}*/
