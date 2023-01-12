8fb2f74ff7063a494d7634391e5cd77941413d5b
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-01-12 12:30:35 +0800

    Update CCP ver 1 for school

---
+++ school monkaS/Project1/School Notes.cpp
@@ -0,0 +1,38 @@
+//program 3 escape sequence 
+#include <iostream>
+using namespace std;
+
+		int main() 
+		{
+		    cout << "My \tName is \nLaphii.";
+		    cout << "I study \nCIT1003";
+		    return 0;
+        }
+
+/*
+output
+My      Name is
+Laphii. I study 
+CIT1003
+*/
+
+//program 2 variable - 
+#include <iostream>
+using namespace std;
+
+		int main() {
+		int age;
+		cout << "How old are you\n";
+		cin >> age;
+		cout << "you are " << age << " years old";
+		return 0;
+}
+
+//program 1 simple command line
+#include <iostream>
+using namespace std;
+
+		int main() {
+		cout << "hello!";
+		return 0;
+		}
