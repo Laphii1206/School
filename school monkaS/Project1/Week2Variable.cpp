b3bcc8ac8cd8168941227eabfd8652602c7012cf
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-01-12 13:23:12 +0800

    Update Week2Variable.cpp

--- school monkaS/Project1/Week2Variable.cpp
+++ school monkaS/Project1/Week2Variable.cpp
@@ -1,1 +1,15 @@
+//program 6 - variable - computer memory (store data) 
+//syntax to create a variable:
+//type variableName
+#include <iostream>
+using namespace std;
 
+  int main(){
+	int age=18; //integer declaration and initialization
+	age = age + 2; //assignment + reading 
+	char letter; //character
+	float pocketMoney; //real number/floating point number
+	double balance; //real number - wider range than float 
+	bool isStudent; //yes/no
+	return 0;
+  }
