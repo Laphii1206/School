69adc8e6cdbe6b24086178647eeec3f3fa46bbec
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-01-12 13:41:44 +0800

    Update Week2Variable.cpp

--- school monkaS/Project1/Week2Variable.cpp
+++ school monkaS/Project1/Week2Variable.cpp
@@ -7,9 +7,13 @@ using namespace std;
   int main(){
 	int age=18; //integer declaration and initialization
 	age = age + 2; //assignment + reading 
+	cout << "Age = " << age;
 	char letter; //character
-	float pocketMoney; //real number/floating point number
-	double balance; //real number - wider range than float 
-	bool isStudent; //yes/no
+	letter = 'w'; //character must be in single quote 
+	cout << "\nLetter is " << letter;
+	float pocketMoney=727.72; //real number/floating point number
+	double balance=1000.0000; //real number - wider range than float 
+	bool isStudent=1; // yes/no (1 = true) (0 = false) anything in bool that is not 0 means true/1
+	cout<<"Are you a student?" << isStudent; 
 	return 0;
   }
