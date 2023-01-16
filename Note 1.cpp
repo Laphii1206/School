236ccc8905e0169764c02d83aa8629934cb29968
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-01-16 22:11:31 +0800

    Update Note 1.cpp

--- Note 1.cpp
+++ Note 1.cpp
@@ -17,3 +17,30 @@ using namespace std;
 	cout<<"\nAre you a student?" << isStudent; 
 	return 0;
   }
+
+//what if statement
+#include <iostream>
+using namespace std;
+int main() {
+	int age;
+	cout << "How old are you? ";
+	cin >> age;
+	if(age>=21)
+		cout << "You are an adult.";
+	else
+		cout << "You are not an adult.";
+	return 0;
+}
+
+//math
+int main() {
+	int totalWeight=178, totalHeight,weight10=50;
+	int a = 5, b=10;
+	a++; //a=a+1	
+	a = a + 5; //a+=5;
+	b = b - 6;
+	totalWeight = totalWeight + weight10;
+	totalWeight +=  weight10;
+	cout << b << endl;
+	return 0;
+}
