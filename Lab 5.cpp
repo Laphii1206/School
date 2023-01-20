06becf6f079115ee23f4dc56a20afaee8e56fda8
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-01-20 09:55:33 +0800

    Update Lab 5.cpp

--- Lab 5.cpp
+++ Lab 5.cpp
@@ -15,3 +15,22 @@ int main(){
   
 return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "Insert the user score" << endl;
+    cin >> num;
+    if (num>=80)
+    cout << "Pass and Good";
+    else if (num>=50 && num<=79)
+    cout << "Pass";
+    else if (num>=45 && num<=49)
+    cout << "Fail and Resit";
+    else 
+    cout << "Fail";
+    return 0;
+    }
+
