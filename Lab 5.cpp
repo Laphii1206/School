d491442081e8acd677fe85208820540dff8c667e
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-01-31 11:25:47 +0800

    Update Lab 5.cpp

--- Lab 5.cpp
+++ Lab 5.cpp
@@ -100,7 +100,9 @@ int main() {
     int num;
     cout << "Insert a number between 1 to 50" << endl;
     cin >> num;
-    if(num < 1 || num > 50)
+    if (num == (int)num) //convert to integer
+    cout << "This is not a integer" << endl;
+    else if(num < 1 || num > 50)
         cout << "Invalid range" << endl;
     else if (num % 2 == 0) {
         cout << num << " is an even" << endl;
@@ -118,6 +120,7 @@ int main() {
     }
     return 0;
 }
+
 
 //exercise that lecture given
 #include <iostream>
