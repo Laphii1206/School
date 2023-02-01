8575b213aff0c956c1b5526f46a2f3295d57db88
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-02-01 22:08:46 +0800

    Update Lab6.cpp

--- Lab/Lab6.cpp
+++ Lab/Lab6.cpp
@@ -18,3 +18,63 @@ int main() {
 	}
 	return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int selection;
+	cout << "Select a number between 1 to 5" << endl;
+	cin >> selection;
+	switch (selection) {
+	case 1: cout << "Congratulation on winning Waffle Maker" << endl; break;
+	case 2: cout << "Congratulation on winning Hair dryer" << endl; break;
+	case 3: cout << "Congratulation on winning Toaster" << endl; break;
+	case 4: cout << "Congratulation on winning Wireless headset" << endl; break;
+	case 5: cout << "Congratulation on winning RM 100 cash voucher" << endl; break;
+	default: cout << "No such choice...";
+	}
+	return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int selection,menu,amount;
+	double price,totalPrice;
+	cout << "Select a ice-cream from the menu" << endl
+		<< "1.Singlecone 12.5 \n2.Double cone 17.9 \n3.Triple cone 20.9 \n4.Banana Split 27.9" << endl;
+	cin >> selection;
+	cout << "Insert the amount of quantity" << endl;
+	cin >> amount;
+	switch (selection) {
+	case 1: price = 12.5; break;
+	case 2: price = 17.9; break;
+	case 3: price = 20.9; break;
+	case 4: price = 27.9; break;
+	default: cout << "No such choice...";
+	}
+	totalPrice = price * amount;
+	cout << "Total Price is RM" << totalPrice;
+	return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	char letter;
+	cout << "Insert a letter" << endl;
+	cin >> letter;
+	switch (letter) {
+	case 'a': case 'A':
+	case 'e': case 'E':
+	case 'i': case 'I':
+	case 'o': case 'O':
+	case 'u': case 'U':
+		cout << letter << " is a vowel" << endl; break;
+	default: cout << letter << " is a consonant" << endl;
+	}
+	return 0;
+}
