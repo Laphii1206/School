d8e9a9533eb18be993341fac9b0665831ddbcaae
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-02-03 08:41:05 +0800

    Update Lab 7.cpp

--- Lab/Lab 7.cpp
+++ Lab/Lab 7.cpp
@@ -38,6 +38,7 @@ using namespace std;
 
 int main(){
 double balance;
+int selection;
 cout << "Welcome to Laphii Bank System" << endl;
 cout << "\nEnter the deposit amount to open saving account (minimum is $250)" << endl;
 cin >> balance;
@@ -46,8 +47,16 @@ cout << "The minimum deposit for a saving account is $250" <<endl
 cout << "\nEnter the deposit amount to open saving account (minimum is $250)" <<endl;
 cin >> balance;
 }
+cout << "1. Deposit money \n2. Withdraw money \n3. Check balance" << endl
+cout << "Selection your option (1 - 3, 0 to quit): ";
+cin >> selection;
+switch (selection){
+case 1;
+case 2;
+case 3: cout << "Balance in the saving account: $" << balance << endl;
+default: cout << "Invalid options";
 
-
+}
 
 
 
