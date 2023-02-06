a2842dd33fec0d0b4d6e79716d2f172008eb960b
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-02-06 22:30:55 +0800

    Update Assignment2.cpp

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -6,18 +6,19 @@ int main() {
 	int selection;
 	double deposit, withdraw;
 	cout << "Welcome to Laphii Bank System" << endl << endl
-	   	 << "Enter the deposit amount to open saving account (minimum is $250): ";
+		<< "Enter the deposit amount to open saving account (minimum is $250): ";
 	cin >> balance;
 	while (balance < 250) {
 		cout << "The minimum deposit for a saving account is $250" << endl
-			 << "Enter the deposit amount to open saving account (minimum is $250): ";
+			<< "Enter the deposit amount to open saving account (minimum is $250): ";
 		cin >> balance;
 	}
+	cout << endl;
 	do {
-		cout << "1. Deposit money" << endl
-			 << "2. Withdraw money" << endl
-			 << "3. Check balance" << endl
-		     << "Selection your option (1 - 3, 0 to quit): ";
+		cout<< "1. Deposit money" << endl
+			<< "2. Withdraw money" << endl
+			<< "3. Check balance" << endl
+			<< "Selection your option (1 - 3, 0 to quit): ";
 		cin >> selection;
 		switch (selection) {
 		case 1: cout << endl << "How much would you like to deposit? ";
@@ -29,17 +30,17 @@ int main() {
 			else {
 				cout << "Transaction accepted." << endl
 					<< "Amount deposited: $" << deposit << endl;
-					balance = deposit + balance;
+				balance = deposit + balance;
 				cout << "Balance in the Savings account: $" << balance << endl << endl;
 			}
-		; break;
-		case 2: cout << endl << "How much do you like to withdraw? "; 
+			; break;
+		case 2: cout << endl << "How much do you like to withdraw? ";
 			cin >> withdraw;
 			if (withdraw > balance) {
 				cout << "Insufficient money in the account to withdraw $" << withdraw << " Account balance is $" << balance << endl
 					<< "Transaction declined." << endl << endl;
 			}
-			else if (withdraw < 0){ 
+			else if (withdraw < 0) {
 				cout << "Invalid input. Withdrawal must be positive." << endl
 					<< "Transaction declined. " << endl << endl;
 			}
@@ -50,11 +51,10 @@ int main() {
 				cout << "Balance in the Savings account: $" << balance << endl << endl;
 			}
 			break;
-		case 3: cout << endl << "Balance in the saving account: $" << balance << endl <<endl; break;
+		case 3: cout << endl << "Balance in the saving account: $" << balance << endl << endl; break;
 		case 0: cout << endl << "Thank you for using the system."; break;
 		default: cout << endl << "Invalid options" << endl; break;
 		}
 	} while (selection != 0);
-			return 0;
+	return 0;
 }
-
