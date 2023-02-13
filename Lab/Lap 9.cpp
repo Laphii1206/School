104d87c35fbfaff2635dbf750d25f401c6f0c44d
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-02-13 21:22:22 +0800

    Create Lap 9.cpp

---
+++ Lab/Lap 9.cpp
@@ -0,0 +1,14 @@
+//Question 1
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x,y,z;
+	for (x = 1;x <= 12;x++) {
+		cout << endl;
+		for (y = 1;y <= 12;y++)
+		
+		cout << x << " * " << y << " = " << x*y<< endl;
+	}
+	return 0;
+}
