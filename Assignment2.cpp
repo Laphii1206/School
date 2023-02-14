4e55ff3cb7eedcbb601209bb076498ad0c0a70c5
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-02-14 14:05:36 +0800

    Update Assignment2.cpp

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -2,21 +2,15 @@
 using namespace std;
 
 double doOpenAccount(double balance) {
-	cout << "Enter the deposit amount to open saving account (minimum is $250): ";
-	cin >> balance;
 	while (balance < 250) {
 		cout << "The minimum deposit for a saving account is $250" << endl
 			<< "Enter the deposit amount to open saving account (minimum is $250): ";
 		cin >> balance;
 	}
-	return 0;
+	return balance;
 }
 
 double doDeposit(double deposit, double balance) {
-	cout << endl << "How much do you like to withdraw? ";
-			cin >> withdraw;
-	cout << endl << "How much would you like to deposit? ";
-			cin >> deposit;
 	if (deposit < 0) {
 		cout << "Invalid input. Deposit must be positive." << endl
 			<< "Transaction declined." << endl << endl;
@@ -27,7 +21,7 @@ double doDeposit(double deposit, double balance) {
 		balance = deposit + balance;
 		cout << "Balance in the Savings account: $" << balance << endl << endl;
 	}
-	return 0;
+	return balance;
 }
 
 double doWithdraw(double withdraw, double balance) {
@@ -43,9 +37,13 @@ double doWithdraw(double withdraw, double balance) {
 		cout << "Transaction accepted." << endl
 			<< "Amount withdrawn: $" << withdraw << endl;
 		balance = balance - withdraw;
-		cout << "Balance in the Savings account: $" << balance << endl << endl;
 	}
-	return 0;
+	cout << "Balance in the Savings account: $" << balance <<endl;
+	return balance;
+}
+
+void showBalance(double balance) {
+	cout << "Balance in the Savings account: $" << balance <<endl <<endl;
 }
 
 int main() {
@@ -53,6 +51,8 @@ int main() {
 	int selection;
 	double deposit, withdraw;
 	cout << "Welcome to The Bank System" << endl << endl;
+	cout << "Enter the deposit amount to open saving account (minimum is $250): ";
+	cin >> balance;
 	doOpenAccount(balance);
 	cout << endl;
 	do {
@@ -62,9 +62,18 @@ int main() {
 			<< "Selection your option (1 - 3, 0 to quit): ";
 		cin >> selection;
 		switch (selection) {
-		case 1:	doDeposit(deposit, balance); break;
-		case 2: doWithdraw(withdraw, balance); break;
-		case 3: cout << showBalance(balance); break;
+		case 1: {
+			cout << endl << "How much would you like to deposit? ";
+			cin >> deposit;
+			balance = doDeposit(deposit, balance);} break;
+		case 2: {
+			cout << endl << "How much do you like to withdraw? ";
+			cin >> withdraw;
+			balance = doWithdraw(withdraw, balance);
+			cout << endl;} break;
+		case 3: 
+			cout << endl;
+			showBalance(balance); break;
 		case 0: cout << endl << "Thank you for using the system."; break;
 		default: cout << endl << "Invalid options" << endl; break;
 		}
