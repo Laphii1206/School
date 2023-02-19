d1587588c3b504172418bacf18e29beb257faef0
Laphii <91127087+Laphii1206@users.noreply.github.com>
2023-02-19 20:26:42 +0800

    Amazing loop that work normally

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -6,86 +6,117 @@ double doDeposit();
 double doWithdraw();
 void showBalance();
 void menuSelection();
+void welcome();
+void closeSystem();
 
 double balance;
+char confirmation, confirmation2;
+
+void welcome() {
+	cout << "Welcome to The Bank System" << endl << endl;
+}
+
+void closeSystem() {
+	cout << "Thank you for using the system.";
+}
 
 double doOpenAccount() {
-    cout << "Enter the deposit amount to open saving account (minimum is $250): ";
-    cin >> balance;
+	cout << "Enter the deposit amount to open saving account (minimum is $250): ";
+	cin >> balance;
 	while (balance < 250) {
-    cout << "The minimum deposit for a saving account is $250" << endl
-	     << "Enter the deposit amount to open saving account (minimum is $250): ";
+		cout << "The minimum deposit for a saving account is $250" << endl
+			<< "Enter the deposit amount to open saving account (minimum is $250): ";
 		cin >> balance;
 	}
+	cout << endl;
 	return balance;
 }
 
 double doDeposit() {
-    double deposit;
-    cout << "How much would you like to deposit? ";
-	cin >> deposit;
-	if (deposit < 0) {
-		cout << "Invalid input. Deposit must be positive." << endl
-			<< "Transaction declined." << endl;
+	double deposit;
+	do {
+		do {
+			cout << endl << "How much would you like to deposit? ";
+			cin >> deposit;
+			if (deposit < 0) {
+				cout << "Invalid input. Deposit must be positive." << endl
+					<< "Transaction declined." << endl;
+			}
+			else {
+				cout << "Confirm to deposit $" << deposit << "? (Y/N): ";
+				cin >> confirmation;
+				confirmation = toupper(confirmation);
+			}
+			} while (confirmation != 'Y');
+			cout << endl << "Transaction accepted." << endl
+				<< "Amount deposited: $" << deposit << endl;
+			balance += deposit;
+			cout << "Balance in the Savings account: $" << balance << endl;
+			cout << endl << "Would you like to make another deposit? (Y/N): ";
+			cin >> confirmation2;
+			confirmation2 = toupper(confirmation2);
+		} while (confirmation2 == 'Y');
+		return balance;
 	}
-	else {
-		cout << "Transaction accepted." << endl
-			<< "Amount deposited: $" << deposit << endl;
-		balance += deposit;
-		cout << "Balance in the Savings account: $" << balance << endl;
-	}
-	return balance;
-}
 
 double doWithdraw() {
-    double withdraw;
-    	cout << "How much do you like to withdraw? ";
-		cin >> withdraw;
-	if (withdraw > balance) {
-		cout << "Insufficient money in the account to withdraw $" << withdraw << " Account balance is $" << balance << endl
-			<< "Transaction declined." << endl;
-	}
-	else if (withdraw < 0) {
-		cout << "Invalid input. Withdrawal must be positive." << endl
-			<< "Transaction declined. " << endl;
-	}
-	else {
-		cout << "Transaction accepted." << endl
+	double withdraw;
+	do {
+		do {
+			cout << endl << "How much do you like to withdraw? ";
+			cin >> withdraw;
+			if (withdraw > balance) {
+				cout << "Insufficient money in the account to withdraw $" << withdraw << " Account balance is $" << balance << endl
+					<< "Transaction declined." << endl;
+			}
+			else if (withdraw < 0) {
+				cout << "Invalid input. Withdrawal must be positive." << endl
+					<< "Transaction declined. " << endl;
+			}
+			else {
+				cout << "Confirmation to withdraw $" << withdraw << "? (Y/N): ";
+				cin >> confirmation;
+				confirmation = toupper(confirmation);
+			}
+		} while (confirmation != 'Y');
+		cout << endl << "Transaction accepted." << endl
 			<< "Amount withdrawn: $" << withdraw << endl;
-		balance-=withdraw;
-	}
-	cout << "Balance in the Savings account: $" << balance << endl;
+		balance -= withdraw;
+		cout << "Balance in the Savings account: $" << balance <<endl;
+		cout << endl << "Would you like to make another withdraw? (Y/N): ";
+		cin >> confirmation2;
+		confirmation2 = toupper(confirmation2);
+	} while (confirmation2 == 'Y');
 	return balance;
 }
 
 void showBalance() {
-	cout << "Balance in the Savings account: $" << balance << endl;
+	cout << endl << "Balance in the Savings account: $" << balance << endl;
 }
 
-void menuSelection(){
-    int selection;
-    do {
-		cout << "1. Deposit money" << endl
+void menuSelection() {
+	int selection;
+	do {
+		cout<< "1. Deposit money" << endl
 			<< "2. Withdraw money" << endl
 			<< "3. Check balance" << endl
 			<< "Selection your option (1 - 3, 0 to quit): ";
 		cin >> selection;
-		cout << endl;
 		switch (selection) {
 		case 1: balance = doDeposit(); break;
 		case 2: balance = doWithdraw(); break;
 		case 3: showBalance(); break;
-		case 0: cout << "Thank you for using the system."; break;
-		default: cout << "Invalid options" << endl; break;
+		case 0: break;
+		default: cout << endl << "Invalid options" << endl; break;
 		}
 		cout << endl;
 	} while (selection != 0);
 }
 
 int main() {
-	cout << "Welcome to The Bank System" << endl << endl;
+	welcome();
 	doOpenAccount();
-	cout << endl;
 	menuSelection();
+	closeSystem();
 	return 0;
 }
